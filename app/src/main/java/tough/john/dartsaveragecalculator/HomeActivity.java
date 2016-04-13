a136282b5e57438fc7e0d1222e045bc60df7a80f
Tough <JOTO@local.tmvse.com>
2016-04-13 16:51:17 +0100

    adding source of android app

---
+++ app/src/main/java/tough/john/dartsaveragecalculator/HomeActivity.java
@@ -0,0 +1,65 @@
+package tough.john.dartsaveragecalculator;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        final Button button = (Button) findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                EditText numberOfDartsView = (EditText) findViewById(R.id.editTextNumberOfDarts);
+                String numberOfDartsString = numberOfDartsView.getText().toString();
+
+                EditText remainingScoreView = (EditText) findViewById(R.id.editTextRemainingScore);
+                String remainingScoreString = remainingScoreView.getText().toString();
+
+                if (numberOfDartsString.matches("") || remainingScoreString.matches("")) {
+                    showWarningDialog(R.string.title_alert_dialog, R.string.message_alert_empty_fields);
+                } else {
+                    float numberOfDarts = Integer.parseInt(numberOfDartsString);
+                    float remainingScore = Integer.parseInt(remainingScoreString);
+
+                    if (numberOfDarts < 6) {
+                        showWarningDialog(R.string.title_alert_dialog, R.string.message_alert_invalid_number_of_darts);
+
+                    } else if (remainingScore > 501 || remainingScore < 0) {
+                        showWarningDialog(R.string.title_alert_dialog, R.string.message_alert_invalid_score);
+                    } else {
+                        final float DARTS_PER_THROW = 3;
+                        final float LEG_TOTAL = 501;
+
+                        float threeDartAverage = (( (LEG_TOTAL - remainingScore) / numberOfDarts ) * DARTS_PER_THROW );
+
+                        TextView threeDartAverageTextView = (TextView) findViewById(R.id.textView3DartAverage);
+                        threeDartAverageTextView.setText(String.format(Locale.UK, "%.2f", threeDartAverage));
+                    }
+                }
+            }
+        });
+    }
+
+    private void showWarningDialog(int title, int message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+        builder.setMessage(message)
+                .setTitle(title)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                }).show();
+    }
+}
